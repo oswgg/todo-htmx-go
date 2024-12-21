ac1bcaca2c09e1b1fdc5d77a35fd8fad8b2abf3a
oswgg <oswaldo.gg0520@gmail.com>
2024-12-21 03:14:45 -0600

    feat: genera la coneccion con la bbdd

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,24 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/oswgg/todo-htmx/internal/handlers"
 	"github.com/oswgg/todo-htmx/internal/repositories"
 	"github.com/oswgg/todo-htmx/internal/service"
 	"net/http"
 )
 
+var connection *sql.DB
+
+func Init() {
+	var err error
+	dsn := "root:rootOGG040520.dev@tcp(localhost:3306)/go_test?parseTime=true"
+	connection, err = sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
 func main() {
-	taskRepo := repositories.NewMockTaskRepository()
+	Init()
+	taskRepo := repositories.NewMariadbTaskRepository(connection)
 	taskService := service.NewTaskService(taskRepo)
 	taskHandler := handlers.NewTaskHandler(taskService)
 
 	root := http.NewServeMux()
 
 	root.HandleFunc("POST /task", taskHandler.Create)
-	root.HandleFunc("PUT /task/{id}/toggle", taskHandler.ToggleTask)
+	//root.HandleFunc("PUT /task/{id}/toggle", taskHandler.ToggleTask)
 	root.HandleFunc("GET /task/update/{id}", taskHandler.UpdateView)
-	root.HandleFunc("POST /task/update/{id}", taskHandler.Update)
+	//root.HandleFunc("POST /task/update/{id}", taskHandler.Update)
 	root.HandleFunc("GET /", taskHandler.List)
 
 	server := &http.Server{
